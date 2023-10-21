e690a2851621495108c3dde0d1ccb80869421e80
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-10-21 23:50:27 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+from misskey import Misskey
+import os
+import time
+
+hiragana = ['ぁ', 'あ', 'ぃ', 'い', 'ぅ', 'う', 'ぇ', 'え', 'ぉ', 'お', 'か', 'が', 'き', 'ぎ', 'く', 'ぐ', 'け', 'げ', 'こ', 'ご', 'さ', 'ざ', 'し', 'じ', 'す', 'ず', 'せ', 'ぜ', 'そ', 'ぞ', 'た', 'だ', 'ち', 'ぢ', 'っ', 'つ', 'づ', 'て', 'で', 'と', 'ど', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ば', 'ぱ', 'ひ', 'び', 'ぴ', 'ふ', 'ぶ', 'ぷ', 'へ', 'べ', 'ぺ', 'ほ', 'ぼ', 'ぽ', 'ま', 'み', 'む', 'め', 'も', 'ゃ', 'や', 'ゅ', 'ゆ', 'ょ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'ゎ', 'わ', 'ゐ', 'ゑ', 'を', 'ん','ー']
+
+max = random.randint(1, 5)
+post_text = ""
+for i in range (max):
+    post_text = post_text + random.choice(hiragana)
+
+print(post_text)
+while True:
+    try:
+        # Misskey
+        misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
+        misskey_token = os.environ.get("MISSKEY_TOKEN")
+        api = Misskey(misskey_address)
+        api.token = misskey_token
+        api.notes_create(text=post_text)
+    except:
+        time.sleep(300)
+    else:
+        break
