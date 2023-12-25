53054f5f9610dad062fb2ad383d3332305ab9752
瑞井 箕月 <6161.k.iga@gmail.com>
2023-12-25 23:43:08 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 from misskey import Misskey
 import os
+from atproto import Client, models
 import time
 
 hiragana = ['ぁ', 'あ', 'ぃ', 'い', 'ぅ', 'う', 'ぇ', 'え', 'ぉ', 'お', 'か', 'が', 'き', 'ぎ', 'く', 'ぐ', 'け', 'げ', 'こ', 'ご', 'さ', 'ざ', 'し', 'じ', 'す', 'ず', 'せ', 'ぜ', 'そ', 'ぞ', 'た', 'だ', 'ち', 'ぢ', 'っ', 'つ', 'づ', 'て', 'で', 'と', 'ど', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ば', 'ぱ', 'ひ', 'び', 'ぴ', 'ふ', 'ぶ', 'ぷ', 'へ', 'べ', 'ぺ', 'ほ', 'ぼ', 'ぽ', 'ま', 'み', 'む', 'め', 'も', 'ゃ', 'や', 'ゅ', 'ゆ', 'ょ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'ゎ', 'わ', 'ゐ', 'ゑ', 'を', 'ん','ー']
@@ -19,6 +20,17 @@ while True:
         api = Misskey(misskey_address)
         api.token = misskey_token
         api.notes_create(text=post_text)
+    except:
+        time.sleep(300)
+    else:
+        break
+
+while True:
+    try:
+        # Bluesky
+        bluesky = Client()
+        bluesky.login(str(os.environ.get("BLUESKY_MAIL_ADDRESS")),str(os.environ.get("BLUESKY_PASSWORD")))
+        bluesky.send_post(post_text)
     except:
         time.sleep(300)
     else:
